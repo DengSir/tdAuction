0c4e1a8df02efc5003d7e44cb15f776fee6e6bd7
Dencer <tdaddon@163.com>
2021-02-01 18:22:17 +0800

    update

--- Localization/enUS.lua
+++ Localization/enUS.lua
@@ -1,8 +1,66 @@
-
 local L = LibStub('AceLocale-3.0'):NewLocale('tdAuction', 'enUS', true)
-if not L then return end
+if not L then
+    return
+end
 
---@localization(locale=""enUS", format="lua_additive_table", table-name="L", same-key-is-true=true)@
+-- @localization(locale=""enUS", format="lua_additive_table", table-name="L", same-key-is-true=true)@
 
---@debug@
---@end-debug@
+-- @debug@
+-- @import@
+
+L['Enable ALT-CTRL click to buyout'] = true
+L['Auto open price list'] = true
+L['Scan price mode'] = true
+L['Scan all'] = true
+L['Scan one page'] = true
+L['Enable ALT to sell'] = true
+L['Default stack size'] = true
+L['Full'] = true
+L['Default auction duration'] = true
+L['Ignore'] = true
+L['Duration no deposit'] = true
+L['When no price, use merchant price multiply by'] = true
+L['Start price discount'] = true
+L['Database'] = true
+L['Clear database'] = true
+L['You are sure to clear the database'] = true
+L['Tooltip'] = true
+L['Merchant price'] = true
+L['Auction price'] = true
+L['Disenchant price'] = true
+L['When pressed SHIFT, to dislay ...'] = true
+L['Total price'] = true
+L['Single price'] = true
+L['Time'] = true
+L['Bid price'] = true
+L['Unit price'] = true
+
+L['Show disenchant info'] = true
+L['Nerver'] = true
+L['Pressed SHIFT'] = true
+L['Always'] = true
+
+L['Full scan'] = true
+L['Start scan'] = true
+L['Next available time:'] = true
+L['in about %s'] = true
+L['Full scaning, elapsed time: %s'] = true
+L['Now'] = true
+L['Unknown'] = true
+L['New'] = true
+L['Update'] = true
+
+L['Count'] = true
+L['Price'] = true
+L['Use merchant price x%d'] = true
+L['No price'] = true
+L['Use history price'] = true
+L['Choose other price'] = true
+
+L['Start auction:'] = true
+
+L.TOOLTIP_PREFIX_VENDO = 'Vendo'
+L.TOOLTIP_PREFIX_AUCTION = 'Auction'
+L.TOOLTIP_PREFIX_DISENCHANT = 'Disenchant'
+-- @end-import@
+-- @end-debug@
