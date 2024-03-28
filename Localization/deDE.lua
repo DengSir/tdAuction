f5af7215a54b571bf5215372cce2e156821943dc
Dencer <tdaddon@163.com>
2024-03-28 16:22:17 +0800

    feat: update locale

--- Localization/deDE.lua
+++ Localization/deDE.lua
@@ -1,5 +1,6 @@
 
-local L = LibStub('AceLocale-3.0'):NewLocale('tdAuction', 'deDE')
+local L = LibStub('AceLocale-3.0'):NewLocale(..., 'deDE')
 if not L then return end
 
---@localization(locale=""deDE", format="lua_additive_table", table-name="L", same-key-is-true=true)@
+-- @locale:language=deDE@
+-- @end-locale@
--- Localization/zhTW.lua
+++ Localization/zhTW.lua
@@ -1,5 +1,6 @@
 
-local L = LibStub('AceLocale-3.0'):NewLocale('tdAuction', 'zhTW')
+local L = LibStub('AceLocale-3.0'):NewLocale(..., 'zhTW')
 if not L then return end
 
---@localization(locale=""zhTW", format="lua_additive_table", table-name="L", same-key-is-true=true)@
+-- @locale:language=zhTW@
+-- @end-locale@
